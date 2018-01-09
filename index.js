4d5b67cc2bc4bae2979c339806052e3126f40d1b
JulianOrtiz94 <julian.ortiz81@gmail.com>
2018-01-09 12:33:07 -0500

    render -> element

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo");
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = document.getElementById('app')
+
+const holaMundo = <h1>Hola mundo!</h1>
+// console.log("Hola mundo");
+ReactDOM.render(holaMundo , App)
